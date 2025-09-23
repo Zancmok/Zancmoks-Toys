2ebce6bf595d3e6ad6d16825563fb40e2ce0b911
Zancmok <145902998+Zancmok@users.noreply.github.com>
2025-09-23 09:09:32 +0000

    just do fucking date kurac blyet

--- SQL/emso_checker.sql
+++ SQL/emso_checker.sql
@@ -2,14 +2,14 @@ SET max_sp_recursion_depth = 255;
 
 DROP PROCEDURE IF EXISTS calculate_control_number;
 delimiter $$
-CREATE PROCEDURE calculate_control_number(INOUT emso INT, OUT control_number INT)
+CREATE PROCEDURE calculate_control_number(IN emso BIGINT, OUT control_number INT)
 BEGIN
 	DECLARE total_sum INT DEFAULT 0;
 	
 	DECLARE i INT DEFAULT 0;
 	WHILE i < 12
 	DO
-		SET total_sum = total_sum + (emso / POW(10, 12 - i) % 10) * (7 - i % 6);
+		SET total_sum = total_sum + (emso DIV POW(10, 12 - i) % 10) * (7 - i % 6);
 		
 		SET i = i + 1;
 	END WHILE;
@@ -29,12 +29,22 @@ END;
 $$
 delimiter ;
 
-DROP FUNCTION IF EXISTS is_emso_valid;
+DROP FUNCTION IF EXISTS is_leap_year
 delimiter $$
-CREATE FUNCTION is_emso_valid(emso INT)
+CREATE FUNCTION is_leap_year(leap_year INT)
 RETURNS BOOL
 BEGIN
-	-- validate date here
+	RETURN ((leap_year % 4 = 0) AND (leap_year % 100 != 0)) OR (leap_year % 400 = 0);
+END;
+$$
+delimiter ;
+
+DROP FUNCTION IF EXISTS is_emso_valid;
+delimiter $$
+CREATE FUNCTION is_emso_valid(emso BIGINT)
+RETURNS BOOL
+BEGIN
+	-- here datum magic
 	
 	DECLARE control_number INT;
 	CALL calculate_control_number(emso, control_number);
